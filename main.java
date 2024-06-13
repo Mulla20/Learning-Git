f56b1dcfd1b09c6ede9b9397e60924c9ee475774
Mulla20 <ikraammulla20@gmail.com>
2024-06-13 12:57:53 -0400

    Added new main.java file

---
+++ main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+
+        //Print
+        System.out.println('T'); //Character print
+        System.out.println("This is a print statement for a String"); //String Print S.
+   
+}
+
+    
